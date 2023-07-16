c6d4e4b50e0537dcf5d569663b4d5a46aeea0fef
Rubenszinho <samuelrubenscontato@gmail.com>
2023-07-16 12:01:21 -0300

    implement functional and imperative algorithms

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# data-analysis-imperative-functional
+# Assignment 2
 This repository contains a Java project focused on data analysis using both imperative and functional programming paradigms. The project aims to explore different approaches to data analysis and compare the advantages and disadvantages of imperative and functional programming in this context.
+
+## Version 1
+
+Implementation in Java using streams and without the use of loops. In this version, the presence of any loop structure in the program will result in a zero score for this item. The objective is to obtain:
+
+- The sum of the "Active" value for all countries where the "Confirmed" value is greater than or equal to n1.
+- Among the n2 countries with the highest "Active" values, obtain the "Deaths" value for the n3 countries with the lowest "Confirmed" values.
+- The n4 countries with the highest "Confirmed" values, in alphabetical order.
+
+## Version 2
+
+Implementation in Java using loops and without the use of streams. In this version, functional programming concepts should not be used. The objective is to obtain the same results described in Version 1.
---
+++ imperativo-v1/Main.java
@@ -0,0 +1,128 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        try {
+            List<DataEntry> dataEntries = readDataEntriesFromFile("dados.csv");
+
+            Scanner scanner = new Scanner(System.in);
+            int n1 = scanner.nextInt();
+            int n2 = scanner.nextInt();
+            int n3 = scanner.nextInt();
+            int n4 = scanner.nextInt();
+            scanner.close();
+
+            if (n1 < 0 || n2 < 0 || n3 < 0 || n4 < 0) {
+                throw new IllegalArgumentException("Os números inseridos não são válidos!");
+            }
+
+            // Soma de "Active" de todos os países em que "Confirmed" é maior ou igual a n1
+            int activeSum = 0;
+            for (DataEntry entry : dataEntries) {
+                if (entry.getConfirmed() >= n1) {
+                    activeSum += entry.getActive();
+                }
+            }
+
+            // n2 países com maiores valores de "Active"
+            List<DataEntry> topActiveCountries = new ArrayList<>(dataEntries);
+            Collections.sort(topActiveCountries, Comparator.comparingInt(DataEntry::getActive).reversed());
+            topActiveCountries = topActiveCountries.subList(0, Math.min(n2, topActiveCountries.size()));
+
+            // "Deaths" dos n3 países com menores valores de "Confirmed" entre os n2 países
+            // com maiores valores de "Active"
+            List<DataEntry> lowestConfirmedCountries = new ArrayList<>(topActiveCountries);
+            Collections.sort(lowestConfirmedCountries, Comparator.comparingInt(DataEntry::getConfirmed));
+            int limit = Math.min(n3, Math.min(n2, lowestConfirmedCountries.size()));
+            if (limit > topActiveCountries.size()) {
+                limit = topActiveCountries.size();
+            }
+            lowestConfirmedCountries = lowestConfirmedCountries.subList(0, limit);
+            List<Integer> deathsList = new ArrayList<>();
+            for (DataEntry entry : lowestConfirmedCountries) {
+                deathsList.add(entry.getDeaths());
+            }
+
+            // n4 países com os maiores valores de "Confirmed" em ordem alfabética
+            List<DataEntry> topConfirmedEntries = new ArrayList<>(dataEntries);
+            Collections.sort(topConfirmedEntries, Comparator.comparingInt(DataEntry::getConfirmed).reversed());
+            topConfirmedEntries = topConfirmedEntries.subList(0, Math.min(n4, topConfirmedEntries.size()));
+
+            List<String> topConfirmedCountries = new ArrayList<>();
+            for (DataEntry entry : topConfirmedEntries) {
+                topConfirmedCountries.add(entry.getCountry());
+            }
+            Collections.sort(topConfirmedCountries);
+
+            System.out.println(activeSum);
+            deathsList.forEach(System.out::println);
+            topConfirmedCountries.forEach(System.out::println);
+
+        } catch (IOException e) {
+            System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Um erro inesperado ocorreu: " + e.getMessage());
+        }
+    }
+
+    private static List<DataEntry> readDataEntriesFromFile(String fileName) throws IOException {
+        List<DataEntry> dataEntries = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+
+                String[] parts = line.split(",");
+                if (parts.length < 5) {
+                    throw new IOException("Dados insuficientes na linha!");
+                }
+                String country = parts[0];
+                int confirmed = Integer.parseInt(parts[1]);
+                int deaths = Integer.parseInt(parts[2]);
+                int active = Integer.parseInt(parts[4]);
+                DataEntry entry = new DataEntry(country, confirmed, deaths, active);
+                dataEntries.add(entry);
+            }
+        }
+        return dataEntries;
+    }
+
+    private static class DataEntry {
+        private String country;
+        private int confirmed;
+        private int deaths;
+        private int active;
+
+        public DataEntry(String country, int confirmed, int deaths, int active) {
+            this.country = country;
+            this.confirmed = confirmed;
+            this.deaths = deaths;
+            this.active = active;
+        }
+
+        public String getCountry() {
+            return country;
+        }
+
+        public int getConfirmed() {
+            return confirmed;
+        }
+
+        public int getDeaths() {
+            return deaths;
+        }
+
+        public int getActive() {
+            return active;
+        }
+    }
+}
